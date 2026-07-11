151b3dd72a4ff83d86403d1c7e7ef1b95f547cd1
Aftabul Islam <toaihimel@gmail.com>
2026-07-11 18:11:53 +0600

    feat: install mongodb/laravel-mongodb and configure connection settings

--- config/database.php
+++ config/database.php
@@ -114,6 +114,19 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGO_DSN'),
+            'host' => env('MONGO_HOST', '127.0.0.1'),
+            'port' => env('MONGO_PORT', 27017),
+            'database' => env('MONGO_DATABASE', 'restoraerp'),
+            'username' => env('MONGO_USERNAME', ''),
+            'password' => env('MONGO_PASSWORD', ''),
+            'options' => [
+                'database' => env('MONGO_AUTH_DATABASE', 'admin'),
+            ],
+        ],
+
     ],
 
     /*
