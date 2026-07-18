1de848fafa9885440ae41db64cf02cd0065c18bf
Aftabul Islam <toaihimel@gmail.com>
2026-07-18 12:37:44 +0600

    feat: externalize demo user credentials to environment configuration

--- config/app.php
+++ config/app.php
@@ -123,4 +123,7 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'demo_username' => env('DEMO_USERNAME', 'admin@demo.com'),
+    'demo_password' => env('DEMO_PASSWORD', 'password'),
+
 ];
